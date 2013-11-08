cb490a5cde4009c8933c8de5493de9e4248e733e
LeoGajardo <leonardo@gajardo.com.br>
2013-11-08 20:41:58 -0200

    Finish the Http File Server

--- Learn Node/HttpFileServer.js
+++ Learn Node/HttpFileServer.js
@@ -2,11 +2,7 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function (req, res) {
-	var src = fs.createReadStream(process.argv[2]);
-	var dest;
-	src.pipe(dest);
-	res.end(dest);
+	fs.createReadStream(process.argv[2]).pipe(res);
 });
-
 server.listen(8000);
 
