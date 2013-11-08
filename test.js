4f4c90be1b09598bf8ee657a899bf2cd2b8aa95c
LeoGajardo <leonardo@gajardo.com.br>
2013-11-08 16:37:43 -0200

    Adding npm and heroku configuration files

--- test.js
+++ test.js
@@ -8,10 +8,11 @@ function onRequest8888 (request, response) {
 
 function onRequest123 (request, response) {
 	response.writeHead(200,{'Content-Type': 'text/plain'});
-	response.write('I\'m Port 123 fuck!');
+	response.write('I\'m Port 3333 bitch!');
 	response.end();
 }
 
 
 http.createServer(onRequest123).listen(3333);
 http.createServer(onRequest8888).listen(8888);
+console.log('Ouvindo em localhost:3333 e localhost:8888');
